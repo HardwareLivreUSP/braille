eb29a43c7440840b2bc7051890f8b4f9c19f0670
Lucas Oshiro <lucasseikioshiro@gmail.com>
2018-10-18 21:42:38 -0300

    Parser

--- Arduino/braille/parser.ino
+++ Arduino/braille/parser.ino
@@ -1,7 +1,17 @@
 #include "parser.hpp"
 
+static void cmd1(uint16_t arg) {
+    
+}
+
+static void cmd2(uint16_t arg) {
+    
+}
+
 void parse_and_execute(uint16_t raw) {
-    const void (*funcs[]) (uint16_t);
-    uint8_t cmd = raw >> 12;
+    void (*funcs[]) (uint16_t) =  {cmd1, cmd2};
+
+    uint8_t  cmd = raw >> 12;
     uint16_t arg = raw & 0x0FFF;
+    funcs[cmd](arg);
 }
